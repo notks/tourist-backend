4e49e6e05c21daa1976d152d71df5db0d3430a2d
notks <kemal.selimbasic.2016@gmail.com>
2021-11-14 00:05:16 +0100

    finishing up

--- src/main/java/com/example/tourist/Constants/Constants.java
+++ src/main/java/com/example/tourist/Constants/Constants.java
@@ -2,5 +2,5 @@ package com.example.tourist.Constants;
 
 public class Constants {
     public static final String API_SECURITY_KEY="mMHO3QDwsnV1eI7OEjna";
-    public static final long TOKEN_VALIDITY=2*60*60*1000;
+    public static final long TOKEN_VALIDITY=2*60*60*1000*100;
 }
